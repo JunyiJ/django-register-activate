db72573a06ffb1b93bf44067058cd1ed53a11691
JunyiJ <jiaojunyi90@gmail.com>
2016-08-20 10:32:01 -0400

    Update Example settings.py. Thanks to Billu

--- docs/Example settings.py
+++ docs/Example settings.py
@@ -16,7 +16,7 @@ import register_activate
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #Add the register_activate path
-register_activate_dir=os.path.dirname(os.path.dirname(os.path.abspath(register_activate.__file__)))
+register_activate_dir=os.path.dirname(os.path.dirname(os.path.abspath('register_activate.__file__')))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
